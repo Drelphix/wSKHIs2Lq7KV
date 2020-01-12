7581e761bdbe81ea442a217b64f276dfd7ccd4bb
Drelphix <alexeydemeshko@gmail.com>
2020-01-12 03:52:22 +0300

    Ньютон

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,8 +9,16 @@ public class Application {
             new Out().OutGaps(data.gaps.get(i), data.points[i]);
         }
         for (int i = 0; i < data.points.length; i++) {
-            data.arrLagranj[i] = new Calculations().CalcLagranj(data.gaps.get(i), data.yx, data.lagranj, data.points[i]);
+            int j = 0;
+            for (int k = 0; k < data.steps.length; k++) {
+                if (data.gaps.get(i)[0] == data.steps[k]) {
+                    j = k;
+                    break;
+                }
+            }
+            data.arrLagranj[i] = new Calculations().CalcLagranj(data.gaps.get(i), data.yx, j, data.lagranj, data.points[i]);
             new Out().OutLagranj(data.points[i], data.arrLagranj[i]);
+            System.out.println("\nНьютон для " + data.points[i] + " = " + new Calculations().CalcNewton(data.steps, data.yx, data.newton, data.points[i], data.step));
         }
 
     }
--- src/main/java/Calculations.java
+++ src/main/java/Calculations.java
@@ -36,26 +36,63 @@ public class Calculations {
         return gap;
     }
 
-    public double CalcLagranj(double[] steps, double[] yx, int lagranj, double value) {
+    public double CalcLagranj(double[] steps, double[] yx, int start, int lagranj, double value) {
         double answer = 0.0;
         double temp;
-        for (int i = 0; i < lagranj; i++) {
+        for (int i = 0; i <= lagranj; i++) {
             temp = 1;
-            for (int j = 0; j < lagranj; j++) {
+            for (int j = 0; j <= lagranj; j++) {
                 if (i != j) {
                     temp *= ((value - steps[j]) / (steps[i] - steps[j]));
                 }
             }
-            temp *= yx[i];
-            answer += temp;
+            answer += temp * yx[start];
+            start++;
         }
         return answer;
     }
 
-    public double CalcNewton(double[] steps, double[] yx, int newton) {
-        double answer = 0.0;
+    public double CalcNewton(double[] steps, double[] yx, int newton, double point, double step) {
+        double[][] mas = new double[newton + 2][newton + 1];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < newton + 1; j++) {
+                if (i == 0)
+                    mas[i][j] = steps[j];
+                else if (i == 1)
+                    mas[i][j] = yx[j];
+            }
+        }
+        int m = newton;
+        for (int i = 2; i < newton + 2; i++) {
+            for (int j = 0; j < m; j++) {
+                mas[i][j] = mas[i - 1][j + 1] - mas[i - 1][j];
+            }
+            m--;
+        }
 
-        return answer;
+        double[] dy0 = new double[newton + 1];
+
+        for (int i = 0; i < newton + 1; i++) {
+            dy0[i] = mas[i + 1][0];
+        }
+
+        double res = dy0[0];
+        double[] xn = new double[newton];
+        xn[0] = point - mas[0][0];
+
+        for (int i = 1; i < newton; i++) {
+            double ans = xn[i - 1] * (point - mas[0][i]);
+            xn[i] = ans;
+            ans = 0;
+        }
+
+        int m1 = newton + 1;
+        int fact = 1;
+        for (int i = 1; i < m1; i++) {
+            fact = fact * i;
+            res = res + (dy0[i] * xn[i - 1]) / (fact * Math.pow(step, i));
+        }
+        return res;
     }
 
     private double[] CalcTable(double[] table) {
